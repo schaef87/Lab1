a1f68b968945749721020194599e9aa4029645e6
Justin Schaefer <jds_06@hotmail.com>
2015-02-02 21:46:19 -0600

    Fixed? some stuff....
    maybe.

--- Text.cpp
+++ Text.cpp
@@ -9,7 +9,7 @@ Text::Text(const char* charSeq = ""){
 }
 
 Text::Text(const Text& other){
-	bufferSize = strlen(Text) + 1;
+	bufferSize = strlen(other) + 1;
 	buffer = new char[bufferSize];
 
 	other = buffer;
@@ -24,7 +24,7 @@ Text::clear(){
 }
 
 Text::showStructure(){
-
+cout << buffer;
 }
 
 Text::toUpper(const Text& other){
@@ -32,17 +32,29 @@ Text::toUpper(const Text& other){
 	buffer = new char[bufferSize];
 
 	int x = 0;
-	while(x < bufferSize){
-		if(charSeq > 96){
+	while(bufferSize[x]){
+		if(buffer[x] > 96 && buffer[x] < 123){
 			buffer[x] = buffer[x] - 32;
-			x++
+			x++;
 		} else {
-			x++
-		}
-	}
+			x++;
+		} //End of IF
+	}  //End of WHILE
 
 }
 
 Text::toLower(){
+	bufferSize = strlen(Text) + 1;
+	buffer = new char[bufferSize];
+
+	int x = 0;
+	while(bufferSize[x]){
+		if(buffer[x] > 64 && buffer[x] < 91){
+			buffer[x] = buffer[x] + 32;
+			x++;
+		} else {
+			x++;
+		} //End of IF
+	}  //End of WHILE
 
 }
